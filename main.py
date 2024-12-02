5faa2f05c22af4a35597d898ebb62dd9e5d0eff8
mehdi-sellam <mehdisellam03@gmail.com>
2024-12-02 09:48:32 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+import requests
+
+
 app = FastAPI()
 
 # Define the Pydantic model for city
@@ -12,17 +15,27 @@ class City(BaseModel):
 # In-memory database (list of cities)
 db: List[City] = []
 
-@app.get("/")
-def index():
-    return {'Hello': 'world'}
+@app.get("/hello/{'world'}")
+def index1(world):
+    return {'hello' : 'world'}
 
 @app.get("/cities")
-def get_cities():
-    return db
+def get_cities2():
+    results = []
+    headers = {
+        "X-Api-Key": "eOuHlh7/rbQWCg04C3pzBw==OLtokvYuIhzZui0v"  # Replace with your actual API key
+    }
+    for city in db:
+        r = requests.get(f'https://api.api-ninjas.com/v1/worldtime?timezone={city["timezone"]}', headers=headers)
+        if r.status_code == 200:
+            current_time = r.json().get('datetime', 'Unknown time')
+            results.append({'name': city['city'], 'timezone': city['timezone'], 'current_time': current_time})
+    return results
+
 
 @app.get("/cities/{city_id}")
-def get_city(city_id: int):
-    return db[city_id]
+def get_city3(city_id: int):
+    return db[city_id-1]
 
 @app.post("/cities")
 def create_city(city: City):
@@ -34,3 +47,9 @@ def delete_city(city_id: int):
     deleted_city = db.pop(city_id)  # Remove and return the city
     return {"message": "City deleted", "city": deleted_city}
         
+
+
+
+
+
+
